ee71176f1eec8222242cd51640ec3e752ce8e915
Mar <55612765+LiuetenantMar@users.noreply.github.com>
2020-03-31 10:12:17 +0800

    Scripting with Python
    Kaizend Session 3

---
+++ Session 3/lab_file_reverser.py
@@ -0,0 +1,29 @@
+# lab_file_reverser
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Read a file in reverse')
+parser.add_argument('filename', help='the file to read')
+parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+args = parser.parse_args()
+
+# We utilize the try statement to denote that it’s quite possible for an error to happen within the try block
+try:
+	f = open(args.filename)
+	limit = args.limit
+
+# We can handle specific types of errors using the except keyword (we can have more than one).
+except FileNotFoundError as err:
+	print(f"Error: {err}")
+# If there isn’t an error, we want to carry out the code that is in the else block
+else:
+	with f:
+		lines = f.readlines()
+		lines.reverse()
+
+	if args.limit:
+		lines = lines[:args.limit]
+
+	for line in lines:
+		print(line.strip()[::-1])
---
+++ Session 3/lab_sys_argv.py
@@ -0,0 +1,5 @@
+# lab_sys_argv
+
+import sys
+print(f"Positional arguments: {sys.argv[1:]}")
+print(f"First argument: {sys.argv[1]}")
